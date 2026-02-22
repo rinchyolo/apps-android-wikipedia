254bc0d3062dbd8dd1a51a3f78394698b973b72e
Михалевская Карина Анатольевна <KMikhalevskaya@alfabank.ru>
2026-02-22 20:23:37 +0300

    fix kcheckbox

--- app/src/androidTest/java/org/wikipedia/lesson06/homework/Theme.kt
+++ app/src/androidTest/java/org/wikipedia/lesson06/homework/Theme.kt
@@ -2,9 +2,9 @@ package org.wikipedia.lesson06.homework
 
 import androidx.appcompat.widget.AppCompatImageView
 import com.kaspersky.kaspresso.screens.KScreen
+import io.github.kakaocup.kakao.check.KCheckBox
 import io.github.kakaocup.kakao.common.views.KView
 import io.github.kakaocup.kakao.image.KImageView
-import io.github.kakaocup.kakao.switch.KSwitch
 import io.github.kakaocup.kakao.text.KButton
 import io.github.kakaocup.kakao.text.KTextView
 import org.wikipedia.R
@@ -33,11 +33,11 @@ object Theme : KScreen<Theme>() {
         withId(R.id.text_size_seek_bar)
     }
 
-    val buttonFontFamilySansSerif = KView {
+    val buttonFontFamilySansSerif = KButton {
         withId(R.id.button_font_family_sans_serif)
     }
 
-    val buttonFontFamilySerif = KView {
+    val buttonFontFamilySerif = KButton {
         withId(R.id.button_font_family_serif)
     }
 
@@ -46,7 +46,7 @@ object Theme : KScreen<Theme>() {
         isInstanceOf(AppCompatImageView::class.java)
     }
 
-    val checkBoxFocusMode = KSwitch {
+    val checkBoxFocusMode = KCheckBox {
         withId(R.id.theme_chooser_reading_focus_mode_switch)
     }
 
@@ -74,11 +74,11 @@ object Theme : KScreen<Theme>() {
         withId(R.id.button_theme_black)
     }
 
-    val switchMatchSystemTheme = KSwitch {
+    val switchMatchSystemTheme = KCheckBox {
         withId(R.id.theme_chooser_match_system_theme_switch)
     }
 
-    val switchImageDimming = KSwitch {
+    val switchImageDimming = KCheckBox {
         withId(R.id.theme_chooser_dark_mode_dim_images_switch)
     }
 }
